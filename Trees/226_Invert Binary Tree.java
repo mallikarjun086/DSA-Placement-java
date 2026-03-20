9e4cbff40fbf115dfe0790de40dd9e37f5b1c794
mallikarjun086 <galamallikarjun@gmail.com>
2026-03-20 13:23:40 +0530

    Create 226_Invert Binary Tree.java

---
+++ Trees/226_Invert Binary Tree.java
@@ -0,0 +1,43 @@
+/*
+Problem: LeetCode 226 - Invert Binary Tree
+Pattern: Tree
+Difficulty: Easy
+
+Approach:
+- Swap left and right recursively
+
+Time Complexity: O(n)
+Space Complexity: O(h)
+*/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        
+        invertTree(root.left);
+        invertTree(root.right);
+        
+        return root;        
+    }
+}
+    
