53bfed11db001075c363290728981fbaa4a9ffcb
mallikarjun086 <galamallikarjun@gmail.com>
2026-03-20 12:52:06 +0530

    Add solution for searching in rotated sorted array
    Implement binary search for a rotated sorted array.

---
+++ BinarySearch/33_SearchinRotatedSortedArray.java
@@ -0,0 +1,45 @@
+/*
+Problem: LeetCode 33 - Search in Rotated Sorted Array
+Pattern: Modified Binary Search
+Difficulty: Medium
+
+Approach:
+- Identify sorted half
+- Apply binary search
+
+Time Complexity: O(log n)
+Space Complexity: O(1)
+*/
+class Solution {
+
+    public int search(int[] nums, int target) {
+
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target)
+                return mid;
+
+            if (nums[left] <= nums[mid]) {
+
+                if (nums[left] <= target && target < nums[mid])
+                    right = mid - 1;
+                else
+                    left = mid + 1;
+
+            } else {
+
+                if (nums[mid] < target && target <= nums[right])
+                    left = mid + 1;
+                else
+                    right = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+}
