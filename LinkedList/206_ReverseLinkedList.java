83dda319c5f55cfce2a8a0248aa994771d8870e0
mallikarjun086 <galamallikarjun@gmail.com>
2026-03-20 12:57:29 +0530

    Create 206_ReverseLinkedList.java

---
+++ LinkedList/206_ReverseLinkedList.java
@@ -0,0 +1,43 @@
+/*
+Problem: LeetCode 206 - Reverse Linked List
+Pattern: Linked List
+Difficulty: Easy
+
+Approach:
+- Iterative pointer reversal
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if(head==null){
+            return null;
+        }
+        if(head.next==null){
+            return head;
+        }
+        ListNode preNode = null;
+        ListNode currNode = head;
+
+        while(currNode!=null){
+            ListNode nextNode =currNode.next;
+            currNode.next=preNode;
+            preNode = currNode;
+            currNode = nextNode;
+        }
+        head = preNode;
+        
+        return head;
+    }
+    
