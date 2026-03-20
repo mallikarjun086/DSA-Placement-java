e941d85db67e6063184d7dd8282858a71d0756dd
mallikarjun086 <galamallikarjun@gmail.com>
2026-03-20 12:44:47 +0530

    Create 424_LongestRepeatingCharacterReplacement.java

---
+++ SlidingWindow/424_LongestRepeatingCharacterReplacement.java
@@ -0,0 +1,39 @@
+/*
+Problem: LeetCode 424 - Longest Repeating Character Replacement
+Pattern: Sliding Window
+Difficulty: Medium
+
+Approach:
+- Maintain max frequency
+- Adjust window size
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+class Solution {
+    public int characterReplacement(String s, int k) {
+
+        int[] freq = new int[26];
+        int left = 0;
+        int maxFreq = 0;
+        int maxWindow = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+
+            freq[s.charAt(right) - 'A']++;
+            maxFreq = Math.max(maxFreq, freq[s.charAt(right) - 'A']);
+
+            int windowLength = right - left + 1;
+
+            if (windowLength - maxFreq > k) {
+                freq[s.charAt(left) - 'A']--;
+                left++;
+            }
+
+            windowLength = right - left + 1;
+            maxWindow = Math.max(maxWindow, windowLength);
+        }
+
+        return maxWindow;
+    }
+}
