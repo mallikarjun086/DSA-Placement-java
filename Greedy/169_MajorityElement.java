4e0e9cf1654d28e4578e50877ed181e4a7d20bd1
mallikarjun086 <galamallikarjun@gmail.com>
2026-03-20 12:34:30 +0530

    Add Majority Element solution using Boyer-Moore
    Implement Boyer-Moore Voting algorithm to find majority element.

---
+++ Greedy/169_MajorityElement.java
@@ -0,0 +1,32 @@
+/*
+Problem: LeetCode 169 - Majority Element
+Pattern: Boyer-Moore Voting
+Difficulty: Easy
+
+Approach:
+- Track candidate and count
+- Cancel out different elements
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+class Solution {
+    public int majorityElement(int[] nums) {
+        int candidate = 0;
+        int count = 0;
+
+        for(int num : nums) {
+            if(count == 0) {
+                candidate = num;
+            }
+
+            if(num == candidate) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+
+        return candidate;
+    }
+}
