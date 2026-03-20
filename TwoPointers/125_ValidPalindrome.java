31ca29a6eb9743b8ffc61c8645b3c654b5deac0c
mallikarjun086 <galamallikarjun@gmail.com>
2026-03-20 12:36:07 +0530

    Add solution for Valid Palindrome problem
    Implemented a solution for checking if a string is a palindrome by ignoring non-alphanumeric characters and comparing characters from both ends.

---
+++ TwoPointers/125_ValidPalindrome.java
@@ -0,0 +1,28 @@
+/*
+Problem: LeetCode 125 - Valid Palindrome
+Pattern: Two Pointers
+Difficulty: Easy
+
+Approach:
+- Ignore non-alphanumeric characters
+- Compare from both ends
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase().replaceAll("[^a-z0-9]", "");
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;        
+    }
+}
