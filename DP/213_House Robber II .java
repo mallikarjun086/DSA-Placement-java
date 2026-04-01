e5320eaf9c1ecebae60ef5b41f2182b3909e7e68
mallikarjun086 <galamallikarjun@gmail.com>
2026-04-01 11:33:09 +0530

    Create 213_House Robber II .java

---
+++ DP/213_House Robber II .java
@@ -0,0 +1,32 @@
+class Solution {
+    public int rob(int[] nums) {
+        if(nums.length < 2) return nums[0];
+
+        int [] skipLastHouse = new int[nums.length - 1];
+        int [] skipFirstHouse = new int[nums.length - 1];
+
+        for(int i = 0; i<nums.length-1;i++){
+            skipLastHouse[i] =nums[i];
+            skipFirstHouse[i] = nums[i+1];
+        }
+        int lootSkippingLast = robHelper(skipLastHouse);
+        int lootSkippingFirst = robHelper(skipFirstHouse);
+
+        return Math.max(lootSkippingLast,lootSkippingFirst);
+    }
+    public int robHelper(int[] nums) {
+        if(nums.length<2) return nums[0];
+
+        int[] dp = new int[nums.length];
+
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0],nums[1]);
+
+        for(int i = 2;i<nums.length;i++){
+            //core logic
+            dp[i] = Math.max(dp[i-2]+nums[i],dp[i-1]);
+        }
+        return dp[nums.length-1];
+    }
+
+}
