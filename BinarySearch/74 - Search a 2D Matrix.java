065cc2edcee35835ff6e938c5ef78ae248b2c62e
mallikarjun086 <galamallikarjun@gmail.com>
2026-04-06 13:49:19 +0530

    Create 74 - Search a 2D Matrix.java

---
+++ BinarySearch/74 - Search a 2D Matrix.java
@@ -0,0 +1,38 @@
+/*
+Problem: LeetCode 74 - Search a 2D Matrix
+Pattern: Binary Search
+Difficulty: Medium
+
+Approach:
+- Treat matrix as 1D sorted array
+- Apply binary search
+
+Time Complexity: O(log(m * n))
+Space Complexity: O(1)
+*/
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        int left = 0;
+        int right = m * n - 1;
+
+        while(left <= right){
+            int mid = left + (right - left) / 2;
+
+            int row = mid / n;
+            int col = mid % n;
+
+            if(matrix[row][col] == target) return true;
+
+            else if(matrix[row][col] < target)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+
+        return false;
+    }
+}
